566845604396cb5f696bb88df90d6f7a4c463420
Raoul Christian Wograndl <Wograndl.Raoul@outlook.com>
2024-07-20 12:38:06 +0200

    feat: create category queries, constraints

--- programming_phoenix/rumbl/lib/rumbl/multimedia.ex
+++ programming_phoenix/rumbl/lib/rumbl/multimedia.ex
@@ -7,6 +7,7 @@ defmodule Rumbl.Multimedia do
   alias Rumbl.Repo
   alias Rumbl.Accounts
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Multimedia.Category
 
   @doc """
   Returns the list of videos.
@@ -117,5 +118,15 @@ defmodule Rumbl.Multimedia do
   """
   def change_video(%Video{} = video, attrs \\ %{}) do
     Video.changeset(video, attrs)
+  end
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
+  end
+
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
   end
 end
