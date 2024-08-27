59ec11ac3dceb7bc9277825c93d783a533333a64
Raoul Christian Wograndl <Wograndl.Raoul@outlook.com>
2024-08-27 14:57:20 +0200

    feat: learn about ecto queries

--- programming_ecto/music_db/priv/repo/playground.exs
+++ programming_ecto/music_db/priv/repo/playground.exs
@@ -38,7 +38,30 @@ defmodule Playground do
   end
 
   def play do
-    Repo.aggregate("artists", :count, :id)
+    q = 
+      "albums"
+      |> by_artist("Miles Davis")
+      |> with_tracks_longer_than(720)
+      |> title_only()
+
+    Repo.all(q)
+  end
+
+  def by_artist(query, artist_name) do
+    from q in query,
+      join: ar in "artists", on: q.artist_id == ar.id,
+      where: ar.name == ^artist_name
+  end
+
+  def with_tracks_longer_than(query, duration) do
+    from a in query,
+      join: t in "tracks", on: t.album_id == a.id,
+      where: t.duration > ^duration,
+      distinct: true
+  end
+
+  def title_only(query) do
+    from a in query, select: a.title
   end
 
 end
